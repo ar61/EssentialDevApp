ba1981bf08db890adea088e52bf60a59339db01c
deedhero <vectorhashmap@gmail.com>
2024-04-26 18:50:28 -0400

    Remove unused init function

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -188,18 +188,6 @@ struct ItemViewModel {
     let title: String
     let subTitle: String
     let select: () -> Void
-    
-    init(_ item: Any, longDateStyle: Bool, selection: @escaping () -> Void) {
-        if let friend = item as? Friend {
-            self.init(friend: friend, selection: selection)
-        } else if let card = item as? Card {
-            self.init(card: card, selection: selection)
-        } else if let transfer = item as? Transfer {
-            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
-        } else {
-            fatalError("unknown item: \(item)")
-        }
-    }
 }
 
 extension ItemViewModel {
@@ -216,7 +204,6 @@ extension ItemViewModel {
         subTitle = card.holder
         select = selection
     }
-    
 }
 
 extension ItemViewModel {
